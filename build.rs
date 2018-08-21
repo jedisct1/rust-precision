d3a37239f80c16fadaeddf32bee4d80769e74c86
Frank Denis <github@pureftpd.org>
2018-08-21 23:51:52 +0200

    We don't need to save %ebp

--- build.rs
+++ build.rs
@@ -3,5 +3,7 @@ extern crate gcc;
 fn main() {
     gcc::Build::new()
         .file("src/cpucounter.c")
+        .flag("-fomit-frame-pointer")
+        .opt_level(2)
         .compile("cpucounter");
 }
