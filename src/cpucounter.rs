7564a3385fa1961bcf9ab8a789f89ed4ef2c5ef1
Johnnie Birch <45402135+jlb6740@users.noreply.github.com>
2020-02-14 17:40:47 -0800

    Adding support for arm mrc instruction.
    Timing reference is here:
    https://github.com/google/benchmark/blob/v1.1.0/src/cycleclock.h#L116

--- src/cpucounter.rs
+++ src/cpucounter.rs
@@ -1,13 +1,24 @@
 use super::timestamp::*;
-
 pub(crate) struct CPUCounter;
 
 #[cfg(asm)]
 #[inline]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 unsafe fn cpucounter() -> u64 {
     let (low, high): (u64, u64);
     asm!("rdtscp" :  "={eax}" (low), "={edx}" (high) : : "ecx");
     (high << 32) | low
+}
+
+
+// https://github.com/google/benchmark/blob/v1.1.0/src/cycleclock.h#L116
+#[cfg(asm)]
+#[inline]
+#[cfg(any(target_arch = "aarch64"))]
+unsafe fn cpucounter() -> u64 {
+    let (vtm): (u64);
+    asm!("mrs %0, cntvct_el0" : "=r"(vtm));
+    vtm
 }
 
 #[cfg(not(asm))]
