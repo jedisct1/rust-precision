c9f2bce41812cdef372489aa39d6a3d5284d87cb
Johnnie Birch <45402135+jlb6740@users.noreply.github.com>
2020-07-26 01:17:03 -0700

    Update inline assembly to support new syntax (#2)
    * Update inline assembly for x86 to support new syntax
    
    https://github.com/rust-lang/rfcs/blob/master/text/2843-llvm-asm.md
    https://github.com/rust-lang/rfcs/pull/2873
    
    * Add support for Arm

--- src/cpucounter.rs
+++ src/cpucounter.rs
@@ -6,10 +6,20 @@ pub(crate) struct CPUCounter;
 
 #[cfg(asm)]
 #[inline]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 unsafe fn cpucounter() -> u64 {
     let (low, high): (u64, u64);
-    llvm_asm!("rdtscp" :  "={eax}" (low), "={edx}" (high) : : "ecx");
+    asm!("rdtscp", out("eax") low, out("edx") high, out("ecx") _);
     (high << 32) | low
+}
+
+#[cfg(asm)]
+#[inline]
+#[cfg(any(target_arch = "aarch64"))]
+unsafe fn cpucounter() -> u64 {
+    let (vtm): (u64);
+    asm!("mrs {}, cntvct_el0", out(reg) vtm);
+    vtm
 }
 
 #[cfg(not(asm))]
