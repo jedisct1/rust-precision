7030f5af0b616725b2c837be289b6a879735e02b
Johnnie Birch <45402135+jlb6740@users.noreply.github.com>
2020-07-25 19:59:26 -0700

    Add support for Arm

--- src/cpucounter.c
+++ src/cpucounter.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 
+#if defined(__x86_64__) || defined(__amd64__)
 uint64_t cpucounter(void)
 {
     uint64_t low, high;
@@ -9,3 +10,12 @@ uint64_t cpucounter(void)
                          : "%ecx");
     return (high << 32) | low;
 }
+#elif defined(__aarch64__)
+uint64_t cpucounter(void)
+{
+    uint64_t virtual_timer_value;
+    __asm__ __volatile__("mrs %0, cntvct_el0"
+                         : "=r"(virtual_timer_value));
+    return virtual_timer_value;
+}
+#endif
